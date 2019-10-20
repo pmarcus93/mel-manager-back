3e11180c0b047a2c931b01a53fea0b9f70135869
Marcus Pereira <pereiramarcus93@gmail.com>
2019-10-20 10:59:23 -0300

    Adicionando seeds da tabela de evento / usuários.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,45 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $userSeedData = [
+            [
+                'name' => "Lucas Junior Dias de Paula",
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => "Marcus Pereira",
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => "Eduardo César Silva Melo",
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => "Daniel Capanema",
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => "Luciana Mara Freitas",
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => bcrypt('password')
+            ]
+        ];
+
+        DB::table('users')->insert($userSeedData);
+
+        $eventoData = [
+            ['nome' => 'Encontro Gamer De Pará De Minas'],
+            ['nome' => 'FINECOM'],
+            ['nome' => 'Startup Weekend'],
+            ['nome' => 'Semana de Estudos Jurídicos'],
+        ];
+
+        DB::table('evento')->insert($eventoData);
+
     }
 }
