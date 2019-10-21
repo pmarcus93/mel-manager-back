2f441b8184f245be5427a68129837e9d5590d774
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-21 14:40:55 -0300

    Limpando metodos e rotas padrão relacionadas a tabela usuários.

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('empresa')->group(function () {
     Route::get('/', 'EmpresaController@index');
     Route::get('{id}', 'EmpresaController@show');
