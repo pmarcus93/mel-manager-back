5086b0a4a6e32eb644f0aa4a862e73739b786f22
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-22 16:52:04 -0300

    Adicionando relacionamento N:N.

--- app/Telefone.php
+++ app/Telefone.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Telefone extends Model
 {
     protected $table = 'telefone';
+
+    public function empresas()
+    {
+        return $this->belongsToMany('App\Empresa','telefone_empresa','telefone_id','empresa_id');
+    }
 }
