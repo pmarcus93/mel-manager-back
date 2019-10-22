ca984a74cc1c9f4ad59ebdd2a5ea8be765223fe1
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-22 15:48:53 -0300

    Atualização para correção sugerida.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -9,88 +9,63 @@ use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
-
     public function cadastrarUsuario()
     {
-
         try {
             $username = \request('name');
             $email = \request('email');
             $password = \request('password');
-
-            $usuario = User::where('email', $email)->get();
-
-            if ($usuario) {
+            $user = User::where('email', $email)->get();
+            if ($user) {
                 throw new \Exception("Este e-mail já foi cadastrado em nosso sistema. [" . $email . "].");
             }
-
-            $usuario = new User();
-            $usuario->name = $username;
-            $usuario->email = $email;
-            $usuario->password = Hash::make($password);
-
-            DB::beginTransaction();
-            $usuario->save();
-            DB::commit();
-            return MelResponse::success("Usuário cadastrado com sucesso!", $usuario);
-
-
+            $user = new User();
+            $user->name = $username;
+            $user->email = $email;
+            $user->password = Hash::make($password);
+            $user->save();
+            return MelResponse::success("Usuário cadastrado com sucesso!", $user);
         } catch (Exception $e) {
-            DB::rollBack();
             return MelResponse::error("Erro ao cadastrar usuário.", $e->getMessage());
         }
     }
 
     public function editarUsuario()
     {
-
         try {
             $user_id = \request('user_id');
             $username = \request('name');
             $email = \request('email');
             $password = \request('password');
-
-            $usuario = User::find($user_id);
-
-            if (!$usuario) {
+            $user = User::find($user_id);
+            if (!$user) {
                 throw new \Exception("Usuário não encontrado em nosso sistema. [" . $user_id . "].");
             }
-
-            $usuario->id = $user_id;
-            $usuario->name = $username;
-            $usuario->email = $email;
-            $usuario->password = Hash::make($password);
-
-            DB::beginTransaction();
-            $usuario->save();
-            DB::commit();
-            return MelResponse::success("Usuário alterado com sucesso!", $usuario);
-
+            $user->id = $user_id;
+            $user->name = $username;
+            $user->email = $email;
+            $user->password = Hash::make($password);
+            $user->save();
+            return MelResponse::success("Usuário alterado com sucesso!", $user);
         } catch (Exception $e) {
-            DB::rollBack();
             return MelResponse::error("Erro ao editar as informações do usuário.", $e->getMessage());
         }
     }
 
-
     public function retornarUsuarioPorNomeEmail()
     {
         try {
-
             $search = \request('search');
-            $usuario = DB::table('users')
+            $user = DB::table('users')
                 ->where('name', 'like', $search . '%')
                 ->orWhere('email', 'like', '%' . $search . '%')
                 ->get();
-
-            if ($usuario->isEmpty()) {
+            if ($user->isEmpty()) {
                 return MelResponse::error("Nenhum registro encontrado para o valor informado.", $search);
             }
-            return MelResponse::success(null, $usuario);
-
+            return MelResponse::success(null, $user);
         } catch (Exception $e) {
             return MelResponse::error("Não foi possível retornar o usuário informado.", $e->getMessage());
         }
     }
-
 }
