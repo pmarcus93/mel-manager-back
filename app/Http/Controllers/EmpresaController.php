9deb7626e392716528f46a76b2b52578e368323d
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-23 15:36:50 -0300

    Inserido método para editar empresas.

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -19,11 +19,35 @@ class EmpresaController extends Controller
             $empresa = new Empresa();
             $empresa->nome = $empresa_nome;
             $empresa->save();
-            $empresa = Empresa::find($empresa->id);
             $telefones_numeros = Request::input('telefones');
             foreach ($telefones_numeros as $telefone_numero) {
                 $telefone = new Telefone();
                 $telefone->numero = $telefone_numero;
+                $telefone->save();
+                $telefoneAdd[] = $telefone->id;
+            }
+            $empresa->telefones()->sync($telefoneAdd);
+            DB::commit();
+            return MelResponse::success("Empresa cadastrada com sucesso!", $empresa);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return MelResponse::error("Erro ao cadastrar empresa.", $e->getMessage());
+        }
+    }
+
+    public function editarEmpresa()
+    {
+        try {
+            DB::beginTransaction();
+            $empresa_id = Request::input('id');
+            $empresa_nome =Request::input('nome');
+            $empresa = Empresa::find($empresa_id);
+            $empresa->nome = $empresa_nome;
+            $empresa->save();
+            $telefones_ids = Request::input('telefones.*.id');
+            foreach ($telefones_ids as $telefone_id) {
+                $telefone = Telefone::find($telefone_id->id);
+                $telefone->numero = $telefone_id->numero;
                 $telefone->save();
                 $telefoneAdd[] = $telefone->id;
             }
