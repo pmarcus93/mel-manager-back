e21ffd02590423a591521343fb601122b34304fe
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-23 14:50:22 -0300

    Inserido método para cadastrar empresas.

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -3,30 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Response\MelResponse;
-use Illuminate\Http\Request;
 use App\Empresa;
+use App\Telefone;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
 
 class EmpresaController extends Controller
 {
 
-    public function cadastrarEmpresa(Request $request)
+    public function cadastrarEmpresa()
     {
         try {
             DB::beginTransaction();
-            $dados = $request->all();
-            $empresa = Empresa::create($dados);
+            $empresa_nome = Request::input('nome');
+            $empresa = new Empresa();
+            $empresa->nome = $empresa_nome;
+            $empresa->save();
             $empresa = Empresa::find($empresa->id);
-            $empresa->telefones()->sync($empresa['telefone_id']);
+            $telefones_numeros = Request::input('telefones');
+            foreach ($telefones_numeros as $telefone_numero) {
+                $telefone = new Telefone();
+                $telefone->numero = $telefone_numero;
+                $telefone->save();
+                $telefoneAdd[] = $telefone->id;
+            }
+            $empresa->telefones()->sync($telefoneAdd);
             DB::commit();
             return MelResponse::success("Empresa cadastrada com sucesso!", $empresa);
         } catch (\Exception $e) {
             DB::rollBack();
             return MelResponse::error("Erro ao cadastrar empresa.", $e->getMessage());
         }
-
-
     }
-
 
 }
