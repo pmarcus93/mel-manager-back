2194f60a1f43593ed6117b0546dabe6c35b5ca1f
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-24 11:34:13 -0300

    Incluindo relacionamento N:N;

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function telefones()
+    {
+        return $this->belongsToMany('App\Telefone','telefone_users','user_id','telefone_id');
+    }
 }
