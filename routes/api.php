1d4f27f8ada09deee6ce4a0fa2b1b92a5b6028ae
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-24 09:44:43 -0300

    Ajuste na  rota para retornar os dados da empresa pelo ID.

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::prefix('usuario')->group(function () {
 Route::prefix('empresa')->group(function () {
     Route::post('/cadastrar-empresa', 'EmpresaController@cadastrarEmpresa');
     Route::post('/editar-empresa', 'EmpresaController@editarEmpresa');
-    Route::get('/retornar-empresa/{empresa_id}','EmpresaController@retornarEmpresa');
+    Route::get('/retornar-empresa/{id}','EmpresaController@retornarEmpresa');
 });
 
 Route::prefix('evento')->group(function () {
