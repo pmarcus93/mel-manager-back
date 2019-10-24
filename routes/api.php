6d9fb2292602313e91230b0f81c8d63efb8c61f9
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-24 13:55:39 -0300

    Rota para retornar todos os usuários.

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::prefix('usuario')->group(function () {
     Route::post('/editar-usuario', 'UsuarioController@editarUsuario');
     Route::get('/retornar-usuario-nome-email/{search}', 'UsuarioController@retornarUsuarioPorNomeEmail');
     Route::get('/retornar-usuario-id/{id}', 'UsuarioController@retornarUsuarioPorID');
+    Route::get('/retornar-usuarios', 'UsuarioController@retornarTodosUsuarios');
 });
 
 Route::prefix('empresa')->group(function () {
