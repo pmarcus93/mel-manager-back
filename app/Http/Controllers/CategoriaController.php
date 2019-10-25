22502b51f2a44b4ffc47eb327d4096e3a0befe93
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-25 11:27:33 -0300

    Inserindo método para editar categoria.

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -20,4 +20,24 @@ class CategoriaController extends Controller
             return MelResponse::error("Erro ao cadastrar categoria.", $e->getMessage());
         }
     }
+
+    public function editarCategoria()
+    {
+        try {
+            $categoria_id = request('id');
+            $categoria_nome = request('nome');
+            $categoria = Categoria::find($categoria_id);
+
+            if (!$categoria) {
+                throw new \Exception("Categoria não econtrada para edição!");
+            }
+
+            $categoria->nome = $categoria_nome;
+            $categoria->save();
+
+            return MelResponse::success("Categoria alterada com sucesso!", $categoria);
+        } catch (Exception $e) {
+            return MelResponse::error("Erro ao alterar categoria.", $e->getMessage());
+        }
+    }
 }
