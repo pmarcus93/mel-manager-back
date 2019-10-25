85ec9900b7e5a0fdb1c1ac70d6808266d781ae67
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-25 11:35:49 -0300

    Adicionando $fillable e $hidden para protejer o que será retornado.

--- app/Categoria.php
+++ app/Categoria.php
@@ -12,4 +12,22 @@ use Illuminate\Http\Request;
 class Categoria extends Model
 {
     protected $table = 'categoria';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at','updated_at',
+    ];
 }
