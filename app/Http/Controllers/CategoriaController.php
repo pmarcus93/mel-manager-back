0c4386af0be7536c26aa032220fd5b4618f1509b
Eduardo César <eduardo.silva@unyleya.com.br>
2019-10-25 11:18:59 -0300

    Adicionado método para salvar categoria.

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Exception;
+use App\Categoria;
+use App\Response\MelResponse;
 
 class CategoriaController extends Controller
 {
-    //
+    public function cadastrarCategoria()
+    {
+        try {
+            $categoria_nome = request('nome');
+            $categoria = new Categoria();
+            $categoria->nome = $categoria_nome;
+            $categoria->save();
+            return MelResponse::success("Categoria cadastrada com sucesso!", $categoria);
+        } catch (Exception $e) {
+            return MelResponse::error("Erro ao cadastrar categoria.", $e->getMessage());
+        }
+    }
 }
