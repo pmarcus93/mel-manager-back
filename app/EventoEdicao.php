4ead9100a266e6dc98d307dc6ae010306356b25d
Eduardo Silva <educ.ms@outlook.com.br>
2019-11-04 23:34:49 -0300

    Inclusão - softdelete ao model edição de evento.

--- app/EventoEdicao.php
+++ app/EventoEdicao.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EventoEdicao extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'evento_edicao';
 
     public function evento()
