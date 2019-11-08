979091bfa0b2436283e4e5852daff0c277a066f8
Eduardo César de Melo Silva <educ.ms@outlook.com.br>
2019-11-08 15:17:10 -0300

    Adição - método cadastrar evento.

--- app/Business/EventoBusiness.php
+++ app/Business/EventoBusiness.php
@@ -1,10 +1,43 @@
 <?php
 
-
 namespace App\Business;
 
+use App\Evento;
+use App\EventoEdicao;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EventoBusiness
 {
+    /**
+     * Cadastra uma novo evento.
+     * @param Request $request
+     * @return Evento
+     * @throws \Exception
+     */
+    public function cadastrarEvento(Request $request)
+    {
+        $attributes = $request->validate([
+            'user_id' => 'required',
+            'nome' => 'nome'
+        ]);
+
+        /** @var User $usuario */
+        $usuario = User::find($attributes['user_id']);
+
+        if (!$usuario) {
+            throw new \Exception("Não existe usuário no banco de dados com o id informado [" . $attributes['user_id'] . "].");
+        }
+
+        DB::beginTransaction();
+        $evento = new Evento();
+        $evento->nome = $attributes['nome'];
+        $evento->save();
+        $evento->administradores()->attach($usuario->id);
+        DB::commit();
+
+        return $evento;
+    }
 
 }
